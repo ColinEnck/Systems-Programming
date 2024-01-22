17e7b5935193315a957b390659d453431814fd66
ColinEnck <colinenck@gmail.com>
2024-01-22 08:15:53 -0500

    automatic commit

--- huffman_code/huffman.c
+++ huffman_code/huffman.c
@@ -1,8 +1,10 @@
+// TODO:
+    // build binary tree from array of Node's
 /*
 USAGE: ./[exec] flags [input filename] [output filename]
 flags:
-    -e encodes the input file
-    -d decodes the input file
+-e  encodes the input file
+-d  decodes the input file
 Flags are required
 The resulting file is saved as [output filename]
 NOTE: both files must be in a .txt format
@@ -13,8 +15,17 @@ NOTE: both files must be in a .txt format
 #include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include "tree.h"
+#include <string.h>
 
+typedef struct Node
+{
+    char c;
+    int freq;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+void usage(const char* arg0);
 // counts the frequency of each ASCII character
 // and stores it in a 256-long array of Node's
 // to set up for the binary tree
@@ -25,17 +36,15 @@ int main(int argc, char const *argv[])
 {
     if (argc != 4)
     {
-        printf("USAGE: %s [flags] [input filename] [output filename]\n", argv[0]);
-        printf("Flags:\n-e\tencodes the input file\n-d\tdecodes the input file\n");
-        printf("Flags are required\nThe resulting file is saved as [output filename]\n");
-        printf("NOTE: both files must be in .txt format\n");
+        usage(argv[0]);
         return 0;
     }
 
-    int infd, outfd, newsize = 0;
-    off_t size;
-    Node* chart = NULL;
+    int infd, outfd;
+    int newsize = 0;
     char* buffer = NULL;
+    off_t size;
+    Node tmp, *top, *chart = NULL;
 
     if ((infd = open(argv[2], O_RDONLY)) == -1)
     {   
@@ -64,19 +73,41 @@ int main(int argc, char const *argv[])
     }
     close(infd);
 
-    chart = freqs(buffer);
-    qsort(chart, 256, sizeof(*chart), cmpfreqs);
-    for (int i = 0; i < 256; i++)
+    if (strcmp(argv[1], "-e") == 0)
     {
-       if (chart[i].freq != 0)
-            newsize++;
-        else 
-            break;
+        chart = freqs(buffer);
+        qsort(chart, 256, sizeof(*chart), cmpfreqs);
+        for (int i = 0; i < 256; i++)
+        {
+        if (chart[i].freq != 0)
+                newsize++;
+            else 
+                break;
+        }
+        for (int i = 0; i < newsize; i++)
+        {
+            printf("%c\t%d\n", chart[i].c, chart[i].freq);
+        }
+        // TODO: generate the binary tree
+        
+    } else if (strcmp(argv[1], "-d") == 0)
+    {
+        // TODO: decode the text
+    } else {
+        usage(argv[0]);
+        return 0;
     }
-    printf("> %c\n> %c\n> %c\n", chart[0].c, chart[1].c, chart[2].c);
 
-    close(outfd);
+    // close(outfd);
     return 0;
+}
+
+void usage(const char* arg0)
+{
+    printf("USAGE: %s [flags] [input filename] [output filename]\n", arg0);
+    printf("Flags:\n-e\tencodes the input file\n-d\tdecodes the input file\n");
+    printf("Flags are required\nThe resulting file is saved as [output filename]\n");
+    printf("NOTE: both files must be in .txt format\n");
 }
 
 Node* freqs(char text[])
@@ -90,6 +121,7 @@ Node* freqs(char text[])
         chars[i].left = NULL;
         chars[i].right = NULL;
     }
+
     for (int cursor = 0; text[cursor] != '\0'; cursor++)
         for (int c = 0; c < 256; c++)
             if (text[cursor] == c)
--- huffman_code/tree.h
+++
@@ -1,7 +0,0 @@
-typedef struct Node
-{
-    char c;
-    int freq;
-    struct Node* left;
-    struct Node* right;
-} Node;
